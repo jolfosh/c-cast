feb3cfce76ee4eb2a4e3d1d1c0bb8dcf9e5d0660
Josh Chasteen <jchasteen@noria.com>
2024-10-24 16:07:55 -0500

    Added movement controls + to do list

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 It's a simple raycaster in the terminal written in c.
 
-Fixed required terminal size of 80x24.
+Required terminal size of 80x24.
 
-Currently just spins in place.
+## Movement: 
+'w' = foward
+'a' = pan left
+'d' = pan right
+
+## To Do:
+ - [] Implement variable terminal size.
+ - [] Find better solution for movement/controls.
+ - [] Add textures and/or colors.
+ - [] Add another dimension for real 3D.
+
--- c_cast.c
+++ c_cast.c
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <stdio.h>
+#include <conio.h>
 #include <math.h>
 
 const int map[10][10] = 
@@ -16,23 +17,23 @@ const int map[10][10] =
 	{ 1,1,1,1,1,1,1,1,1,1 },
 };
 
-const double PI = 3.14;
+const float PI = 3.14;
 const int PRECISION = 64;
 const int sc_width = 78;
 const int sc_height = 24;
 const int sc_half_width = 39;
 const int sc_half_height = 12;
-const double p_fov = 60; 
-const double p_half_fov = 30;
-double p_angle = 180;
-double p_x = 2;
-double p_y = 2;
-double r_inc = 1;
+const float p_fov = 60; 
+const float p_half_fov = 30;
+float p_angle = 180;
+float p_x = 2;
+float p_y = 2;
+float r_inc = 1;
 
 char sc[24][78] = {' '};
 char buffer[1896] = {' '};
 
-double degree_to_R(int degree) {
+float degree_to_R(int degree) {
 	return (degree * PI / 180);
 }
 
@@ -45,12 +46,12 @@ void clear_sc() {
 }
 
 void cast() {
-	double r_angle = p_angle - p_half_fov;
+	float r_angle = p_angle - p_half_fov;
 	for(int i = 0; i < sc_width; i++) {
-		double r_x = p_x;
-		double r_y = p_y;
-		double r_cos = cos(degree_to_R(r_angle)) / PRECISION;
-		double r_sin = sin(degree_to_R(r_angle)) / PRECISION;
+		float r_x = p_x;
+		float r_y = p_y;
+		float r_cos = cos(degree_to_R(r_angle)) / PRECISION;
+		float r_sin = sin(degree_to_R(r_angle)) / PRECISION;
 		int wall_x = -1;
 		int wall_y = -1;
 		int wall = 0;
@@ -61,12 +62,22 @@ void cast() {
 			wall_y = floor(r_y);
 			wall = map[wall_y][wall_x];
 		}
-		double distance = sqrt(pow(p_x - r_x, 2) + pow(p_y - r_y, 2));
+		float distance = sqrt(pow(p_x - r_x, 2) + pow(p_y - r_y, 2));
 
 		// distance = distance * cos(degree_to_R(r_angle - p_angle));
 
-		double wall_h = floor(sc_half_height / distance);
-		for(int j = (sc_half_height - wall_h); j < (sc_half_height + wall_h); j++){
+		int wall_h = floor(sc_half_height / distance);
+		if (wall_h >= sc_height){
+			wall_h = sc_height - 1;
+		}
+		
+		int j_start = sc_half_height - wall_h;
+		if (j_start < 0)
+		{
+			j_start = 0;
+		}
+
+		for(int j = j_start; j < (sc_half_height + wall_h); j++){
 			sc[j][i] = '#';
 		}
 		for(int j = (sc_half_height + wall_h); j < sc_height; j++){
@@ -90,6 +101,30 @@ void draw() {
 	printf("%.1896s", buffer);
 }
 
+void p_move(){ 
+	float p_cos = cos(degree_to_R(p_angle)) * 0.2;
+	float p_sin = sin(degree_to_R(p_angle)) * 0.2;
+	int c_x = floor(p_x + p_cos);
+	int c_y = floor(p_y + p_sin);
+	if (map[c_y][c_x] == 0) {
+		p_x += p_cos;
+		p_y += p_sin;
+	}
+}
+
+void keypressed() {
+	if(_kbhit()){
+		int k = _getch();
+		if (k == 'a') {
+			p_angle -= 2;	
+		} else if (k == 'd') {
+			p_angle += 2;
+		} else if (k == 'w') {
+			p_move();
+		}
+	}
+}
+
 int main(int argc, char** argv) {
 	r_inc = p_fov / sc_width;
 
@@ -98,8 +133,9 @@ int main(int argc, char** argv) {
 		// Sleep(100);
 		// printf("\033[2J");
 		draw();
+		keypressed();
 		clear_sc();
-		p_angle += 0.05;
+		// p_angle += 0.05;
 	}
 	return 0;
 }
